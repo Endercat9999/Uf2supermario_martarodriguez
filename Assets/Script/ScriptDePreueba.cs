bb9333e73bf6b3f132d0d02609efe72c9d55d233
Endercat9999 <2005martarosa@gmail.com>
2024-04-03 10:33:53 +0200

    array

--- Assets/Script/ScriptDePreueba.cs
+++ Assets/Script/ScriptDePreueba.cs
@@ -11,10 +11,42 @@ public class ScriptDePrueba : MonoBehaviour
     public string nombrePersonaje = "Nombre del personaje";
     private bool interuptor = false;
 
+    public int[] arrayNumber; 
+
+    private int[] arrayNumber2 = new int[6];
+
+    private int[] arrayNumber3  [7, 8, 3, 9];
+
+    private string[] arrayString = new string[7];
+
+    private string[] arrayString2 = {"Hola, adios"};
+
+    private int[,] array2Dimensiones = new int [4, 2];
+
+    private int[,] array2Dimensiones2 = [{7, 8, 65, 0}, {9, 2, 545, 8}];
+
+    public List<string> stringList;
+
+    public List<int> intList = new List<int>(7);
+    public List<int> intList2 = new List<int>(2. 4, 8, 4, 6);
     // Start is called before the first frame update
     void Start()
     {
-        vidasPersonaje = 10;
+        Dabug.Log(arrayNumber[1]);
+
+        Dabug.Log(array2Dimensiones2[0, 1]);
+
+        arrayNumber2[0] = 4;
+
+        array2Dimensiones2[2, 1] = 7777777;
+
+        intList2.Add(10);
+        intList2.Insert(5, 888);
+
+        intList2.RemoveAt(2);
+    
+
+        /*vidasPersonaje = 10;
         numeroDecimal = 7.54f;
         nombrePersonaje = "Mario";
         interuptor = true;
@@ -26,7 +58,7 @@ public class ScriptDePrueba : MonoBehaviour
 
         Debug.Log(nombrePersonaje);
         Debug.Log(vidasPersonaje);
-        Debug.Log(numeroDecimal);
+        Debug.Log(numeroDecimal);*/
     }
 
     // Update is called once per frame
